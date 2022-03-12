3e0dfb7a56badbef2bd37f4cf8f4df69e0edddac
Walid Mohamed <51799013+WalidMohamed1@users.noreply.github.com>
2022-03-12 22:11:59 +0200

    Make a dynamic URL rounting

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home), #Base URL
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('', views.home, name='home'), #Base URL
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk_test>/', views.customer, name='customer'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from accounts.models import *
@@ -22,5 +23,10 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html',{'products':products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')          
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    orders = customer.order_set.all()
+    total_orders = orders.count()
+    context = {'customer':customer, 'orders':orders, 'total_orders':total_orders}
+
+    return render(request, 'accounts/customer.html', context)          
