eedddb78e5e5f7951fe84a3aaf08b355c5ec5b0f
Walid Mohamed <51799013+WalidMohamed1@users.noreply.github.com>
2022-03-15 15:18:32 +0200

    Password Reset Email.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
+from re import template
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -17,6 +19,22 @@ urlpatterns = [
     path('create_order/<str:pk>/', views.createOrder, name='create_order'),
     path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+      name="reset_password"),
+
+    path('reset_password_sent/',
+     auth_views.PasswordResetDoneView.as_view(template_name="accounts/passowrd_reset_sent.html"),
+      name="password_reset_done"),
+
+    path('reset<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/passowrd_reset_form.html"),
+      name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+     auth_views.PasswordResetCompleteView.as_view(template_name="accounts/passowrd_reset_done.html"),
+      name="password_reset_complete"),
 
 
 
