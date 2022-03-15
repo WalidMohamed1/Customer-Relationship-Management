c54312eb088be4d053eddf16ac2b30afa82f56f4
Walid Mohamed <51799013+WalidMohamed1@users.noreply.github.com>
2022-03-15 15:18:32 +0200

    Password Reset Email.

--- crm/settings.py
+++ crm/settings.py
@@ -132,6 +132,15 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'Your email'
+EMAIL_HOST_PASSWORD = 'Your password'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
