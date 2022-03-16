baf08613cc9a8d8fa9d9278d5827b34a4a3f101a
Walid Mohamed <51799013+WalidMohamed1@users.noreply.github.com>
2022-03-16 16:18:23 +0200

    Deploying to Heroku Server with SQlite DB.

--- crm/settings.py
+++ crm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s@ccg6w6k1ch%yg=)y8u+0ye*=omktctb-@hgg!cghp*vj3sgi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['walid-crm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,12 +83,13 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crm',
-        'USER': 'postgres',
-        'PASSWORD': '[,]f,dkj2020',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': 'crm',
+        #'USER': 'postgres',
+        #'PASSWORD': '[,]f,dkj2020',
+        #'HOST': 'localhost',
+        #'PORT': '5432'
     }
 }
 
@@ -126,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATOC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
