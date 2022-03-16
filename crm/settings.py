f7249784aceb47ba83d1aab6ada0c681d48ce5de
Walid Mohamed <51799013+WalidMohamed1@users.noreply.github.com>
2022-03-16 16:55:11 +0200

    Modify the misspelling in settings.py that disables the project to deploy -4- .

--- crm/settings.py
+++ crm/settings.py
@@ -132,14 +132,13 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 #SMTP Configuration
 
