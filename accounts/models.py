33684407135680112aaa613c182e15bc45117ae2
Walid Mohamed <51799013+WalidMohamed1@users.noreply.github.com>
2022-03-18 20:51:47 +0200

    Update customer model

--- accounts/models.py
+++ accounts/models.py
@@ -10,8 +10,6 @@ class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
-    #email = models.CharField(max_length=200, null=True)
-    customer_orders = models.IntegerField(null=True)
     profile_pic = models.ImageField(default="default.png", null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
